ff98824c8e65aacfe03f53b0ac6ea125db06a3a4
zhengtengfei <zhengtengfei@tuandai.com>
2018-10-31 16:33:55 +0800

    chengogng

---
+++ spring-beans/src/test/java/org/springframework/litespring/service/v1/PetStoreService.java
@@ -0,0 +1,7 @@
+package org.springframework.litespring.service.v1;
+
+/**
+ * Created by zhengtengfei on 2018/10/30.
+ */
+public class PetStoreService {
+}
---
+++ spring-beans/src/test/java/org/springframework/litespring/test/v1/ResourceTest.java
@@ -0,0 +1,34 @@
+package org.springframework.litespring.test.v1;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by zhengtengfei on 2018/10/30.
+ */
+public class ResourceTest {
+	@Test
+	public void testClassLoader(){
+		Resource resource = new ClassPathResource("petStore1.xml");
+
+		InputStream in = null;
+		try{
+			in = resource.getInputStream();
+			Assert.assertNotNull(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (in != null)
+					in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
